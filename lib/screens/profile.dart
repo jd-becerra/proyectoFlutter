2364e9f8aada3efae85b577f064440c4d261a281
jd-becerra <jbecerra0@ucol.mx>
2025-10-21 20:48:02 -0600

    Remove unused user model import from profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_flutter/provider.dart';
-import 'package:proyecto_flutter/models/user.dart';
 import 'package:provider/provider.dart';
 import 'package:proyecto_flutter/widgets/title.dart';
 
